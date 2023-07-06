79ba6b42ba2089e5834b43293b8a7287b0451fe3
dahee oh <ekgml4122@gmail.com>
2023-07-06 23:25:40 +0900

    점프 투 스프링부트/2-15/서비스

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 // @RequiredArgsConstructor : questionRepository 속성을 포함하는 생성자를 생성
 // final이 붙은 속성을 포함하는 생성자를 자동으로 생성
@@ -15,13 +20,27 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
-    @GetMapping("/question/list")
+    private final QuestionService questionService;
+
+    @GetMapping("/list")
 //    @ResponseBody  // 템플릿을 사용하기에 @ResponseBody 필요 없음
     public String list(Model model) {
     // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할 (Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다)
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
+
         model.addAttribute("questionList", questionList);
         return "question_list";  // list 메서드에서 question_list.html 템플릿 파일의 이름인 "question_list"를 리턴
     }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";  // 질문 저장후 질문목록으로 이동
+    }
+
 }
